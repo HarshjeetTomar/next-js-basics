5415714d0ae7fe970a41a79d1d8b5292f4bc8e08
Harshjeet <tomarharshjeet@gmail.com>
2024-01-23 20:08:04 +0530

    first commit and app routes completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/Navbar/Navbar'
+import Footer from '@/components/Footer'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +14,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      
+      
+      <body className={inter.className}>
+      <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+      <div className="Container">
+      
+        <Navbar/>
+       {children}
+       <Footer/>
+       </div>
+       </AppRouterCacheProvider>
+        </body>
     </html>
   )
 }
